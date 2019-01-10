beaabecf2f7767d2da02daf59c0b2fdbc30f8117
Edward Faulkner <ef@alum.mit.edu>
2019-01-09 20:12:19 -0500

    [wip] working on transition

--- cards/asset/addon/components/embedded.js
+++ cards/asset/addon/components/embedded.js
@@ -1,8 +1,12 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import layout from '../templates/embedded';
+import { offscreenLeft, dontScale, logoTransition } from './isolated';
 
 export default Component.extend({
+  logoTransition,
+  dontScale,
+  offscreenLeft,
   layout,
   title: computed('content.title', function () {
     return this.get('content.title').toLowerCase();
