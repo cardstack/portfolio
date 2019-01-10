6fba15974949c6ca1314b30d5212b325060ce7a8
Edward Faulkner <ef@alum.mit.edu>
2019-01-10 16:57:05 -0500

    backing out experimental version

--- cards/asset/addon/components/embedded.js
+++ cards/asset/addon/components/embedded.js
@@ -1,12 +1,8 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import layout from '../templates/embedded';
-import { offscreenLeft, dontScale, logoTransition } from './isolated';
 
 export default Component.extend({
-  logoTransition,
-  dontScale,
-  offscreenLeft,
   layout,
   title: computed('content.title', function () {
     return this.get('content.title').toLowerCase();
--- cards/asset/addon/components/isolated.js
+++ cards/asset/addon/components/isolated.js
@@ -1,46 +1,8 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import layout from '../templates/isolated';
-import move from 'ember-animated/motions/move';
-import resize from 'ember-animated/motions/resize';
-import { wait } from 'ember-animated';
-
-export function * offscreenLeft({ receivedSprites }) {
-  yield null;
-  // insertedSprites.forEach(sprite => {
-  //   sprite.startAtPixel({ x: -1 * window.outerWidth })
-  //   move(sprite);
-  // });
-  // removedSprites.forEach(sprite => {
-  //   sprite.applyStyles({ 'z-index': 1000 });
-  //   sprite.endAtPixel({ x: -1 * window.outerWidth });
-  //   move(sprite);
-  // });
-  receivedSprites.forEach(sprite => {
-    move(sprite);
-    resize(sprite);
-    sprite.applyStyles({ 'z-index': 1 });
-  });
-}
-
-export function * logoTransition({ sentSprites }) {
-  yield null;
-  sentSprites.forEach(sprite => {
-    move(sprite);
-    resize(sprite);
-  });
-}
-
-export function * dontScale({ duration, removedSprites }) {
-  if (removedSprites.length > 0) {
-    yield wait(duration);
-  }
-}
 
 export default Component.extend({
-  logoTransition,
-  dontScale,
-  offscreenLeft,
   layout,
   title: computed('content.title', function () {
     return this.get('content.title').toLowerCase();
