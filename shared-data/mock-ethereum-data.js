8bc953843f783df08bd62a037e2fb53d5cfc2b50
Burcu Noyan <burcunoyan@gmail.com>
2019-01-18 16:13:45 -0500

    Mock eth data add

--- shared-data/mock-ethereum-data.js
+++ shared-data/mock-ethereum-data.js
@@ -12,6 +12,7 @@ let models = [
       // "balance": "200895000000000000", // to be derived based on the transactions so the data stays consistent
       "ethereum-address": "0xC3D7FcFb69D168e9339ed18869B506c3B0F51fDE"
     },
+<<<<<<< HEAD
     "relationships": {
       "transactions": {
         "data": [
@@ -30,6 +31,115 @@ let models = [
           {
             "id": "0xe65061bd5f9733d7213f893171075d4cb5cf2ce77fb1b0dbb0ac5a94eb84bf65",
             "type": "ethereum-transactions"
+||||||| merged common ancestors
+    {
+      "id": "0x0c0b1a4b0ff5fbf2124f122b70b5c752e1289e60f376e13ab51865dee747f572",
+      "type": "ethereum-transactions",
+      "attributes": {
+        "transaction-successful": true,
+        "transaction-index": 17,
+        "cumulative-gas-used": 21000,
+        "transaction-data": "0x0",
+        "timestamp": 1547478615,
+        "transaction-nonce": 17,
+        "block-number": 6,
+        "gas-used": 21000,
+        "transaction-from": "0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5",
+        "block-hash": "0x62c48a107a96894248726dba13d114a1760fa9eef5370e98a0651ccb0ba0c41f",
+        "transaction-to": "0xc3d7fcfb69d168e9339ed18869b506c3b0f51fde",
+        "gas-price": "5000000000",
+        "transaction-hash": "0x0c0b1a4b0ff5fbf2124f122b70b5c752e1289e60f376e13ab51865dee747f572",
+        "gas": 90000,
+        "transaction-value": "101000000000000000"
+      },
+      "relationships": {
+        "from-address": {
+          "data": {
+            "id": "0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5",
+            "type": "ethereum-addresses"
+          }
+        },
+        "to-address": {
+          "data": {
+            "id": "0xc3d7fcfb69d168e9339ed18869b506c3b0f51fde",
+            "type": "ethereum-addresses"
+=======
+    {
+      "id": "0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5",
+      "type": "ethereum-addresses",
+      "attributes": {
+        "balance": "100895000000000000",
+        "ethereum-address": "0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5"
+      },
+      "relationships": {
+        "transactions": {
+          "data": [
+            {
+              "id": "0x0c0b1a4b0ff5fbf2124f122b70b5c752e1289e60f376e13ab51865dee747f572",
+              "type": "ethereum-transactions"
+            }
+          ]
+        }
+      },
+    },
+    {
+      "id": "0xaefa57a8b9ddb56229ae57d61559fc2a4c5af0cd",
+      "type": "ethereum-addresses",
+      "attributes": {
+        "balance": "300895000000000000",
+        "ethereum-address": "0xaefa57a8b9ddb56229ae57d61559fc2a4c5af0cd"
+      },
+      "relationships": {
+        "transactions": {
+          "data": [
+            {
+              "id": "0x6efc3a61e5fac700a8d992f4bad4bfa62763601b05b04d27f2f96607ce097259",
+              "type": "ethereum-transactions"
+            },
+            {
+              "id": "0x3252a963fe90697240890b84d2a3fac45b756338027467e2788ad0bb82b1fdc2",
+              "type": "ethereum-transactions"
+            },
+            {
+              "id": "0xe65061bd5f9733d7213f893171075d4cb5cf2ce77fb1b0dbb0ac5a94eb84bf65",
+              "type": "ethereum-transactions"
+            }
+          ]
+        }
+      },
+    },
+    {
+      "id": "0x0c0b1a4b0ff5fbf2124f122b70b5c752e1289e60f376e13ab51865dee747f572",
+      "type": "ethereum-transactions",
+      "attributes": {
+        "transaction-successful": true,
+        "transaction-index": 17,
+        "cumulative-gas-used": 21000,
+        "transaction-data": "0x0",
+        "timestamp": 1547478615,
+        "transaction-nonce": 17,
+        "block-number": 6,
+        "gas-used": 21000,
+        "transaction-from": "0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5",
+        "block-hash": "0x62c48a107a96894248726dba13d114a1760fa9eef5370e98a0651ccb0ba0c41f",
+        "transaction-to": "0xc3d7fcfb69d168e9339ed18869b506c3b0f51fde",
+        "gas-price": "5000000000",
+        "transaction-hash": "0x0c0b1a4b0ff5fbf2124f122b70b5c752e1289e60f376e13ab51865dee747f572",
+        "gas": 90000,
+        "transaction-value": "101000000000000000"
+      },
+      "relationships": {
+        "from-address": {
+          "data": {
+            "id": "0x0f4f2ac550a1b4e2280d04c21cea7ebd822934b5",
+            "type": "ethereum-addresses"
+          }
+        },
+        "to-address": {
+          "data": {
+            "id": "0xc3d7fcfb69d168e9339ed18869b506c3b0f51fde",
+            "type": "ethereum-addresses"
+>>>>>>> Mock eth data add
           }
         ]
       }
@@ -167,6 +277,7 @@ let models = [
           "type": "ethereum-addresses"
         }
       },
+<<<<<<< HEAD
       "to-address": {
         "data": {
           "id": "0xc3d7fcfb69d168e9339ed18869b506c3b0f51fde",
@@ -184,3 +295,10 @@ let balance = models.filter(i => i.type === 'ethereum-transactions')
 address.attributes.balance = balance.toString();
 
 module.exports = models;
+||||||| merged common ancestors
+    }
+  ];
+=======
+    }
+  ];
+>>>>>>> Mock eth data add
