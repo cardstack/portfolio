3ba432ace0a3bdddc5cc2e6c8e27da1c3a898bb1
Will Bagby <bagsbee@gmail.com>
2019-01-23 07:31:40 -0500

    Group assets by currency

--- cards/portfolio/addon/components/isolated.js
+++ cards/portfolio/addon/components/isolated.js
@@ -5,13 +5,15 @@ import { computed } from '@ember/object';
 import { readOnly } from '@ember/object/computed';
 import { roundWithPrecision } from 'portfolio-common/helpers/round-with-precision';
 
+
 export default LiveIsolatedCard.extend({
   layout,
-
   dummyAccount: service(),
   selectedCurrency: service(),
+  store: service(),
 
   currency: readOnly('selectedCurrency.currency'),
+  groupBy: 'wallets',
 
   accountTotal: computed('wallets.[]', 'currency', function() {
     let total = this.content.get('wallets').toArray().reduce((sum, wallet) => {
@@ -27,5 +29,34 @@ export default LiveIsolatedCard.extend({
     let wallets = this.content.get('wallets');
     if (!wallets) { return; }
     return wallets.length;
-  })
+  }),
+
+  assetsByCurrency: computed('content', 'content.wallets.[]', function() {
+    let networks = this.store.peekAll('network').sortBy('title');
+
+    if (!networks) { return; }
+
+    let allAssets = this.store.peekAll('asset');
+
+    let currencyGroups = [];
+
+    networks.forEach(network => {
+      let byNetwork = { network };
+      let assets = allAssets.filter(asset => asset.get('networkId') === network.id );
+      byNetwork.assets = assets;
+      currencyGroups.push(byNetwork);
+    });
+
+    return currencyGroups;
+  }),
+
+  actions: {
+    groupByCurrency() {
+      this.set('groupBy', 'currency');
+    },
+
+    groupByWallet() {
+      this.set('groupBy', 'wallets');
+    }
+  }
 });
