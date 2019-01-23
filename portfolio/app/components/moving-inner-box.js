3a74494b3a6d15a5dba726518f7c1f8f886362f1
Edward Faulkner <ef@alum.mit.edu>
2019-01-23 04:45:25 -0500

    simplify moving-inner-box

--- portfolio/app/components/moving-inner-box.js
+++ portfolio/app/components/moving-inner-box.js
@@ -3,40 +3,25 @@ import move from 'ember-animated/motions/move';
 import resize from 'ember-animated/motions/resize';
 import adjustColor from 'ember-animated/motions/adjust-color';
 import { duration } from './moving-box';
-import opacity from 'ember-animated/motions/opacity';
-import { wait } from 'ember-animated';
 
 export default Component.extend({
   tagName: '',
   duration,
 
-  outer: function * ({ sentSprites }) {
+  transition: function * ({ sentSprites }) {
     sentSprites.forEach(sprite => {
+
+      // Caution! This is a hack. It just happens to be safe in this situation
+      // because we're messing with only the children of sentSprites, which will
+      // all get destroyed at the end of animation regardless.
+      [...sprite.element.children].forEach(element => {
+        element.classList.add('ember-animated-hidden');
+      });
+
       sprite.applyStyles({ 'z-index': 1 });
       move(sprite);
       resize(sprite);
       adjustColor.property('background-color')(sprite);
     });
-  },
-
-  inner: function * ({ receivedSprites, sentSprites, duration }) {
-    sentSprites.forEach(sprite => {
-      move(sprite);
-      opacity(sprite, { to: 0, duration: duration * 0.2 });
-      sprite.applyStyles({ 'z-index': 2 });
-    });
-
-    receivedSprites.forEach(sprite => {
-      let diff = sprite.difference('finalBounds', sprite, 'initialBounds');
-      sprite.translate(diff.dx, diff.dy);
-      sprite.applyStyles({ opacity: 0 });
-    });
-
-    if (receivedSprites.length > 0) {
-      yield wait(duration * 0.75);
-      receivedSprites.forEach(sprite => {
-        opacity(sprite, { from: 0, to: 1, duration: duration * 0.2 });
-      });
-    }
-  },
+  }
 });
