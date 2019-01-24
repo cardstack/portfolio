39c9335fd33ece8c68ae08fbc3813b10fc197c43
Burcu Noyan <burcunoyan@gmail.com>
2019-01-24 13:02:14 -0500

    More test fixes

--- packages/common/addon/components/breadcrumbs.js
+++ packages/common/addon/components/breadcrumbs.js
@@ -1,25 +1,23 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { readOnly, equal, match } from '@ember/object/computed';
+import { equal, match } from '@ember/object/computed';
 import layout from '../templates/breadcrumbs';
 
 export default Component.extend({
   layout,
   tagName: '',
-  userId: readOnly('cardstackData.session.data.authenticated.data.id'),
   isAsset: equal('content.type', 'asset'),
   isTransaction: match('content.type', /.*?-transaction/),
-  toEthAddress: readOnly('content.toAddress.ethereumAddress'),
 
-  assetPath: computed('toEthAddress', function () {
-    let address = this.get('toEthAddress');
+  assetPath: computed('transactionTo', function () {
+    let address = this.content.get('transactionTo');
     if (!address) { return; }
 
     return `/assets/${address}`;
   }),
 
-  assetType: computed('content.type', function () {
-    let type = this.get('content.type');
+  assetType: computed('type', function () {
+    let type = this.content.get('type');
     if (!type || !type.includes('-')) { return; }
 
     let network = type.slice(0, type.indexOf('-')).toLowerCase();
@@ -27,9 +25,9 @@ export default Component.extend({
     return network;
   }),
 
-  abbreviatedAddress: computed('toEthAddress', function () {
-    // TO DO: Currently only using "To Address". Which one to display here???
-    let address = this.get('toEthAddress');
+  abbreviatedAddress: computed('transactionTo', function () {
+    // TO DO: Currently only using "To Address" because more of those are indexed
+    let address = this.content.get('transactionTo');
     if (!address) { return; }
 
     return `${address.slice(0, 6)}...${address.slice(-4)}`;
