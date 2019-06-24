9d850c6919fe93f473c7edceec2a2ccbdbb68af2
Will Bagby <bagsbee@gmail.com>
2019-06-24 11:44:39 -0400

    Add token conversion rates

--- cards/crypto-compare/cardstack/conversion-map.js
+++ cards/crypto-compare/cardstack/conversion-map.js
@@ -1,6 +1,14 @@
-module.exports = {
+const { erc20Tokens } = require('portfolio-utils');
+
+let conversionMap = {
   BTC: ['USD', 'EUR'],
   ETH: ['USD', 'EUR'],
   LTC: ['USD', 'EUR'],
-  ZEC: ['USD', 'EUR'],
+  ZEC: ['USD', 'EUR']
 };
+
+for (let token of erc20Tokens) {
+  conversionMap[token.symbol] = ['USD', 'EUR'];
+}
+
+module.exports = conversionMap;
